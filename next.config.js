4da0abfd0cb7acfed0e604d72634cbec65f3392e
pokketo <pokketoconfig@gmail.com>
2026-02-12 11:30:11 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "bjorn66.com",
-      "6ammart-test.6amdev.xyz",
-      "192.168.50.168",
-      "6ammart-dev.6amdev.xyz",
-      "uatadmin.pokketo.com",
-      'uat.pokketo.com'
-    ], // Add the domain here
+    domains: ['localhost'], // Add the domain here
   },
 };
 
